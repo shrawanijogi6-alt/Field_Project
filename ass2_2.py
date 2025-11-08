8811ac8357dda57484d4047ce565136aa30ec812
shrawanijogi6-alt <shrawanijogi6@gmail.com>
2025-11-08 18:41:57 +0530

    first commit

---
+++ ass2_2.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+print(f"Hello, my name is {name}, I am {age} years old, and I live in {city}.")
